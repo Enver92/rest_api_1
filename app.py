dfb8f71957611efa0c7ba018d6a86242848bc49e
Enver92 <taraqtash@gmail.com>
2018-03-31 14:31:18 +0300

    Edited authentication procedure

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key='some key'
 api = Api(app)
 
+# JWT creates a new endpoint /auth
+jwt = JWT(app, authenticate, identity)
+
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self, name):
+        """
+            A user has to be authenticated to send a GET request
+        """
         item = next(filter(lambda item: item['name']==name, items), None) # returns the first item found
         return {"item": item}, 200 if item else 404
 
